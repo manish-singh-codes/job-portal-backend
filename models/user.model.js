9aebcddefd9ee35ae120b04f32355f6203e787ce
Manish Singh <manishsingh704293@gmail.com>
2025-06-11 18:44:38 +0530

    update profile pending

--- models/user.model.js
+++ models/user.model.js
@@ -2,6 +2,53 @@ import mongoose from "mongoose";
 import { type } from "os";
 
 
+const workExperienceSchema = new mongoose.Schema({
+  companyName: String,
+  designation: String,
+  from: Date,
+  to: Date,
+  description: String,
+});
+
+const educationSchema = new mongoose.Schema({
+  course: String,
+  institute: String,
+  from: Date,
+  to: Date,
+  gpaOrPercentage: String,
+});
+
+const technicalSkillsSchema = new mongoose.Schema({
+  programmingLanguages: [String],
+  frameworksOrLibraries: [String],
+  databases: [String],
+  toolsAndTechnologies: [String],
+});
+
+const softSkillsSchema = new mongoose.Schema({
+  skill: String,
+  level: {
+    type: String,
+    enum: ['Beginner', 'Medium', 'Advance', 'Expert'],
+  },
+});
+
+const certificationSchema = new mongoose.Schema({
+  certificateName: String,
+  instituteName: String,
+  issueDate: Date,
+});
+
+const resumeSchema = new mongoose.Schema({
+  fileName: String,
+  fileUrl: String,
+  uploadedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+
 const userSchema = new mongoose.Schema(
   {
     fullname: {
@@ -48,6 +95,14 @@ const userSchema = new mongoose.Schema(
     },
     otpExpiresAt: {
       type: Date
+    },
+    overView : {
+      workExperience: [workExperienceSchema],
+      education: [educationSchema],
+      technicalSkills: technicalSkillsSchema,
+      softSkills: [softSkillsSchema],
+      certifications: [certificationSchema],
+      resume: resumeSchema,
     }
   },
   { timestamps: true }
